59684fe5fb9b6aadf19985b4a56c170f31e7d31c
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-04 15:16:28 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,53 @@
 variable "vpc_name" {
-  type    = string
+  type        = string
   description = "Name of VPC"
-  default = "cmtr-4ca2aaf4-01-vpc"
+  default     = "cmtr-4ca2aaf4-01-vpc"
 }
 
 variable "subnet1_name" {
-  type    = string
+  type        = string
   description = "Name of subnet1"
-  default = " cmtr-4ca2aaf4-01-subnet-public-a"
+  default     = " cmtr-4ca2aaf4-01-subnet-public-a"
 }
 
 variable "subnet2_name" {
-  type    = string
+  type        = string
   description = "Name of subnet2"
-  default = " cmtr-4ca2aaf4-01-subnet-public-b"
+  default     = " cmtr-4ca2aaf4-01-subnet-public-b"
 }
 
 variable "subnet3_name" {
-  type    = string
+  type        = string
   description = "Name of subnet3"
-  default = " cmtr-4ca2aaf4-01-subnet-public-c"
+  default     = " cmtr-4ca2aaf4-01-subnet-public-c"
 }
 
 variable "subnet1_az" {
-  type    = string
+  type        = string
   description = "AZ subnet1"
-  default = "eu-west-1a"
+  default     = "eu-west-1a"
 }
 
 variable "subnet2_az" {
-  type    = string
+  type        = string
   description = "AZ subnet2"
-  default = "eu-west-1b"
+  default     = "eu-west-1b"
 }
 
 variable "subnet3_az" {
-  type    = string
+  type        = string
   description = "AZ subnet3"
-  default = "eu-west-1c"
+  default     = "eu-west-1c"
 }
 
 variable "igw_name" {
-  type    = string
+  type        = string
   description = "name of internet gateway"
-  default = "cmtr-4ca2aaf4-01-igw"
+  default     = "cmtr-4ca2aaf4-01-igw"
 }
 
 variable "route_table_name" {
-  type    = string
+  type        = string
   description = "Route table name"
-  default = "cmtr-4ca2aaf4-01-rt"
+  default     = "cmtr-4ca2aaf4-01-rt"
 }
