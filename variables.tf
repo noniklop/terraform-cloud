141e4b455561549e98d32959e6ea2f8c63e027ff
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-07 18:15:43 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "ssh_key" {
-  type = string
+  type        = string
   description = "Provides custom public SSH key"
 }
 
 variable "existing_security_group_id" {
-  type = string
+  type        = string
   description = "SG id"
   default     = "sg-0fb26d06c24bb6abf"
 }
 
 variable "existing_subnet_id" {
-  type = string
+  type        = string
   description = "subnet id"
   default     = "subnet-057febbbaaf302154"
 }
