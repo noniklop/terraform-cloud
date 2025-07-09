8809fd59ee280f54e40a4553eb4eb052be0e3a22
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-09 16:35:45 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,23 @@
-variable "vpc_name" {
+variable "iam_group_name" {
+  description = "group name"
   type        = string
-  description = "Name of VPC"
-  default     = "cmtr-4ca2aaf4-01-vpc"
+  default     = "cmtr-4ca2aaf4-iam-group"
 }
 
-variable "subnet1_name" {
+variable "policy_name" {
+  description = "policy name"
   type        = string
-  description = "Name of subnet1"
-  default     = "cmtr-4ca2aaf4-01-subnet-public-a"
+  default     = "cmtr-4ca2aaf4-iam-policy"
 }
 
-variable "subnet2_name" {
+variable "role_name" {
+  description = "role name"
   type        = string
-  description = "Name of subnet2"
-  default     = "cmtr-4ca2aaf4-01-subnet-public-b"
+  default     = "cmtr-4ca2aaf4-iam-role"
 }
 
-variable "subnet3_name" {
+variable "instance_profile" {
+  description = "instance profile"
   type        = string
-  description = "Name of subnet3"
-  default     = "cmtr-4ca2aaf4-01-subnet-public-c"
-}
-
-variable "subnet1_az" {
-  type        = string
-  description = "AZ subnet1"
-  default     = "eu-west-1a"
-}
-
-variable "subnet2_az" {
-  type        = string
-  description = "AZ subnet2"
-  default     = "eu-west-1b"
-}
-
-variable "subnet3_az" {
-  type        = string
-  description = "AZ subnet3"
-  default     = "eu-west-1c"
-}
-
-variable "igw_name" {
-  type        = string
-  description = "name of internet gateway"
-  default     = "cmtr-4ca2aaf4-01-igw"
-}
-
-variable "route_table_name" {
-  type        = string
-  description = "Route table name"
-  default     = "cmtr-4ca2aaf4-01-rt"
+  default     = "cmtr-4ca2aaf4-iam-instance-profile"
 }
