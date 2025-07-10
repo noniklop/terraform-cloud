7808ffafb4faa56b78d08e8ee1a54937e09a7b61
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-10 17:10:39 +0300

    Update network_security.tf

--- network_security.tf
+++ network_security.tf
@@ -1,3 +1,22 @@
+resource "aws_security_group" "empty_sg" {
+  name   = "empty-1488"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 1488
+    to_port     = 1488
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = var.allowed_ip_range
+  }
+}
+
 resource "aws_security_group" "ssh_sg" {
   name   = "cmtr-4ca2aaf4-ssh-sg"
   vpc_id = var.vpc_id
@@ -61,7 +80,7 @@ resource "aws_security_group_rule" "what" {
   to_port           = 65535
   protocol          = "tcp"
   cidr_blocks       = ["10.1.1.0/24"]
-  security_group_id = aws_security_group.public_sg.id
+  security_group_id = aws_security_group.empty_sg.id
 }
 
 resource "aws_network_interface_sg_attachment" "public_instance_ssh" {
