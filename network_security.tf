3bd2c15c2628a396db5ce88a1c4ad8066f3a77ed
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-10 16:50:54 +0300

    Update network_security.tf

--- network_security.tf
+++ network_security.tf
@@ -52,6 +52,15 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
+resource "aws_security_group_rule" "what" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = "10.1.1.0/24"
+  security_group_id = [aws_security_group.public_sg.id]
+}
+
 resource "aws_network_interface_sg_attachment" "public_instance_ssh" {
   security_group_id    = aws_security_group.ssh_sg.id
   network_interface_id = "eni-011e42be3e11a598c"
