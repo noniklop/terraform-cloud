6d406d50afa606db4cd7ca1672652ce4c5dd18bc
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-10 17:01:10 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "allowed_ip_range" {
   type        = list(string)
   default     = ["188.163.9.79/32", "18.153.146.156/32"]
 }
+
+variable "vpc_id" {
+  description = "vpc id"
+  type        = string
+  default     = "vpc-0b4ba9336075da7fd"
+}
