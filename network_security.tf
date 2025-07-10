50166f84d85e2b210205609c4155cb633e355df3
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-10 16:55:19 +0300

    Update network_security.tf

--- network_security.tf
+++ network_security.tf
@@ -58,7 +58,7 @@ resource "aws_security_group_rule" "what" {
   to_port           = 65535
   protocol          = "tcp"
   cidr_blocks       = ["10.1.1.0/24"]
-  security_group_id = [aws_security_group.public_sg.id]
+  security_group_id = aws_security_group.public_sg.id
 }
 
 resource "aws_network_interface_sg_attachment" "public_instance_ssh" {
