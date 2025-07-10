3c6ba77158e83c2235924d515c124526dae5061f
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-10 16:31:26 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "allowed_ip_range" {
   description = "list of IP address range for secure access"
   type        = list(string)
-  default     = ["188.163.9.79/32", "18.153.146.156"]
+  default     = ["188.163.9.79/32", "18.153.146.156/32"]
 }
