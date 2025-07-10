dd19af86c1c460dfa8fe9e10d499e7cf2b66b169
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-10 15:43:01 +0300

    Update and rename main.tf to network_security.tf

---
+++ network_security.tf
@@ -0,0 +1,73 @@
+resource "aws_security_group" "ssh_sg" {
+  name = "cmtr-4ca2aaf4-ssh-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = var.allowed_ip_range
+  }
+}
+
+resource "aws_security_group" "public_sg" {
+  name = "cmtr-4ca2aaf4-public-http-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = var.allowed_ip_range
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name = "cmtr-4ca2aaf4-private-http-sg"
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  ingress {
+    from_port       = -1
+    to_port         = -1
+    protocol        = "icmp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+}
+
+resource "aws_network_interface_sg_attachment" "public_instance_ssh" {
+  security_group_id    = aws_security_group.ssh_sg.id
+  network_interface_id = "eni-011e42be3e11a598c"
+}
+
+resource "aws_network_interface_sg_attachment" "public_instance_http" {
+  security_group_id    = aws_security_group.public_sg
+  network_interface_id = "eni-011e42be3e11a598c"
+}
+
+resource "aws_network_interface_sg_attachment" "private_instance_ssh" {
+  security_group_id    = aws_security_group.ssh_sg.id
+  network_interface_id = "eni-0d97fbb022ff720df"
+}
+
+resource "aws_network_interface_sg_attachment" "private_instance_http" {
+  security_group_id    = aws_security_group.private_sg.id
+  network_interface_id = "eni-0d97fbb022ff720df"
+}
