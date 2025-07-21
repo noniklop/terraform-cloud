2f3402018c40f84fb78ee86b3b4c6395e496b559
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-21 16:12:05 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,23 @@
-variable "vpc_name" {
+variable "aws_region" {
+  description = "aws region"
   type        = string
-  description = "Name of VPC"
-  default     = "cmtr-4ca2aaf4-01-vpc"
+  default     = "eu-west-1"
 }
 
-variable "subnet1_name" {
+variable "project_id" {
+  description = "project id"
   type        = string
-  description = "Name of subnet1"
-  default     = "cmtr-4ca2aaf4-01-subnet-public-a"
+  default     = "cmtr-4ca2aaf4"
 }
 
-variable "subnet2_name" {
+variable "state_bucket" {
+  description = "state bucket"
   type        = string
-  description = "Name of subnet2"
-  default     = "cmtr-4ca2aaf4-01-subnet-public-b"
+  default     = "cmtr-4ca2aaf4-tf-state-1753102282"
 }
 
-variable "subnet3_name" {
+variable "state_key" {
+  description = "state bucket"
   type        = string
-  description = "Name of subnet3"
-  default     = "cmtr-4ca2aaf4-01-subnet-public-c"
-}
-
-variable "subnet1_az" {
-  type        = string
-  description = "AZ subnet1"
-  default     = "eu-west-1a"
-}
-
-variable "subnet2_az" {
-  type        = string
-  description = "AZ subnet2"
-  default     = "eu-west-1b"
-}
-
-variable "subnet3_az" {
-  type        = string
-  description = "AZ subnet3"
-  default     = "eu-west-1c"
-}
-
-variable "igw_name" {
-  type        = string
-  description = "name of internet gateway"
-  default     = "cmtr-4ca2aaf4-01-igw"
-}
-
-variable "route_table_name" {
-  type        = string
-  description = "Route table name"
-  default     = "cmtr-4ca2aaf4-01-rt"
+  default     = "infra.tfstate"
 }
