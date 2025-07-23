e0261bd0e3408aeb33df80e9108e7b8660632721
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-23 11:42:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
-provider "aws" {
-  region = "eu-west-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
