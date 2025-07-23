7dc299b159f9c0804dad971415bafc20c0291ee1
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-23 10:28:52 +0300

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,9 +1,35 @@
-data "terraform_remote_state" "base_infra" {
-  backend = "s3"
+data "aws_vpc" "cmtr-4ca2aaf4-vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["cmtr-4ca2aaf4-vpc"]
+  }
+}
 
-  config = {
-    bucket = var.state_bucket
-    key    = var.state_key
-    region = var.aws_region
+data "aws_subnet" "cmtr-4ca2aaf4-public-subnet-1" {
+  filter {
+    name   = "tag:Name"
+    values = ["cmtr-4ca2aaf4-public-subnet-1"]
+  }
+}
+
+data "aws_security_group" "cmtr-4ca2aaf4-sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["cmtr-4ca2aaf4-sg"]
+  }
+}
+
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+
+  owners = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
