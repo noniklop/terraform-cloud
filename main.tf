4faa02d0d22298c3041699a9ce857e50a1fbebe5
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-25 16:50:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ module "network_security" {
 }
 
 module "application" {
-  source = "./modules/application"
-  ssh_sg = module.network_security.ssh_security_group_id
-  public_http_sg = module.network_security.public_http_security_group_id
+  source          = "./modules/application"
+  ssh_sg          = module.network_security.ssh_security_group_id
+  public_http_sg  = module.network_security.public_http_security_group_id
   private_http_sg = module.network_security.private_http_security_group_id
-  subnet_a_id = module.vpc.subnet_a_id
-  subnet_b_id = module.vpc.subnet_b_id
-  subnet_c_id = module.vpc.subnet_c_id
-  vpc_id = module.vpc.vpc_id
+  subnet_a_id     = module.vpc.subnet_a_id
+  subnet_b_id     = module.vpc.subnet_b_id
+  subnet_c_id     = module.vpc.subnet_c_id
+  vpc_id          = module.vpc.vpc_id
 }
