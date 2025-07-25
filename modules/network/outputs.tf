d2093336a23317a4238c510413a181078ab4e50c
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-25 16:43:26 +0300

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -5,15 +5,15 @@ output "vpc_id" {
 
 output "subnet_a_id" {
   description = "subnet a id"
-  value = aws_subnet.public_subnet_a.id
+  value       = aws_subnet.public_subnet_a.id
 }
 
 output "subnet_b_id" {
   description = "subnet b id"
-  value = aws_subnet.public_subnet_b.id
+  value       = aws_subnet.public_subnet_b.id
 }
 
 output "subnet_c_id" {
   description = "subnet c id"
-  value = aws_subnet.public_subnet_c.id
+  value       = aws_subnet.public_subnet_c.id
 }
