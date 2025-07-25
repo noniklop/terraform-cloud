b19f2b7a15c261c5672e808078ab04c99c1758e0
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-25 11:22:41 +0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
-terraform {
-  required_version = ">= 1.5.7"
+resource "aws_iam_policy" "custom_policy" {
+  name        = "resource-move-demo-policy"
+  description = "Custom role with limited permissions"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = ["ec2:*", "s3:*"], # Replace with specific actions
+        Resource = "*"
+      }
+    ]
+  })
 }
