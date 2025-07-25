ddd2d5c85ae2ae8bba13a8a208f947ef82ead0b6
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-25 16:37:29 +0300

    Create variables.tf

---
+++ modules/application/variables.tf
@@ -0,0 +1,40 @@
+variable "instance_type" {
+  description = "instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "vpc_id" {
+  description = "vpc id"
+  type = string
+}
+
+variable "ssh_sg" {
+  description = "ssh sg id"
+  type = string
+}
+
+variable "public_http_sg" {
+  description = "http sg id"
+  type = string
+}
+
+variable "private_http_sg" {
+  description = "private http sg id"
+  type = string
+}
+
+variable "subnet_a_id" {
+  description = "subnet a id"
+  type = string
+}
+
+variable "subnet_b_id" {
+  description = "subnet b id"
+  type = string
+}
+
+variable "subnet_c_id" {
+  description = "subnet c id"
+  type = string
+}
