dd13205603698e4a7a9fc963341dcb31321cf925
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-25 16:43:17 +0300

    Update variables.tf

--- modules/application/variables.tf
+++ modules/application/variables.tf
@@ -6,35 +6,35 @@ variable "instance_type" {
 
 variable "vpc_id" {
   description = "vpc id"
-  type = string
+  type        = string
 }
 
 variable "ssh_sg" {
   description = "ssh sg id"
-  type = string
+  type        = string
 }
 
 variable "public_http_sg" {
   description = "http sg id"
-  type = string
+  type        = string
 }
 
 variable "private_http_sg" {
   description = "private http sg id"
-  type = string
+  type        = string
 }
 
 variable "subnet_a_id" {
   description = "subnet a id"
-  type = string
+  type        = string
 }
 
 variable "subnet_b_id" {
   description = "subnet b id"
-  type = string
+  type        = string
 }
 
 variable "subnet_c_id" {
   description = "subnet c id"
-  type = string
+  type        = string
 }
