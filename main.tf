565652b3ec61493c56f37ff8386951a8c6a6f3c9
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-25 16:40:59 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
-provider "aws" {
-  region = "eu-west-1"
+module "vpc" {
+  source = "./modules/network"
+}
+
+module "network_security" {
+  source = "./modules/network_security"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "application" {
+  source = "./modules/application"
+  ssh_sg = module.network_security.ssh_security_group_id
+  public_http_sg = module.network_security.public_http_security_group_id
+  private_http_sg = module.network_security.private_http_security_group_id
+  subnet_a_id = module.vpc.subnet_a_id
+  subnet_b_id = module.vpc.subnet_b_id
+  subnet_c_id = module.vpc.subnet_c_id
+  vpc_id = module.vpc.vpc_id
 }
