a2269d12efb07d95de1ba689515227af262b330a
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2025-07-31 10:19:20 +0300

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,3 +1,21 @@
 resource "aws_iam_policy" "name" {
+  name        = "cmtr-4ca2aaf4-iam-policy"
+  description = "This is an example IAM policy imported by Terraform"
 
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:*",
+                "s3:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+
+    EOF
 }
